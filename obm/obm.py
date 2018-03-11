4489b7626ecc42cb459ba89554ab8e883152bab6
wangzi <785692920@qq.com>
2018-03-11 20:27:41 +0800

    Although it is a mess, it runs.

--- obm/obm.py
+++ obm/obm.py
@@ -1,10 +1,11 @@
-from app import create_app, db, cli
-from app.models import User, Post
+# from app import create_app, db, cli
+# from app.models import User, Post
+from app import  create_app
 
 app = create_app()
-cli.register(app)
+# cli.register(app)
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post}
